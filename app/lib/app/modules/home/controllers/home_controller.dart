303992e0be6e29e6150eb7df051abbae0a931f36
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-12-09 11:10:23 +0200

    Get Data

--- app/lib/app/modules/home/controllers/home_controller.dart
+++ app/lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,68 @@
+import 'package:app/app/models/info.dart';
+import 'package:app/app/models/location.dart';
+import 'package:app/app/models/sensor.dart';
+import 'package:app/app/models/sensor_data_values.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+
+import '../../../models/sensor_type.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
-  final count = 0.obs;
+  List<Info> infoList = [];
+
   @override
   void onInit() {
+    loadInfo();
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  Future<void> loadInfo() async {
+    var dio = Dio();
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+    var value = await dio.get('https://data.sensor.community/static/v2/data.json');
 
-  void increment() => count.value++;
+    infoList = (value.data as List).map((e) {
+      return Info(
+        e['id'],
+        e['sampling_rate'],
+        DateTime.parse(e['timestamp']),
+        Location(
+          e['location']['id'],
+          e['location']['latitude'],
+          e['location']['longitude'],
+          e['location']['altitude'],
+          e['location']['country'],
+          e['location']['exact_location'],
+          e['location']['indoor'],
+        ),
+        Sensor(
+          e['sensor']['id'],
+          e['sensor']['pin'],
+          SensorType(
+            e['sensor']['sensor_type']['id'],
+            e['sensor']['sensor_type']['name'],
+            e['sensor']['sensor_type']['manufacturer'],
+          ),
+        ),
+        (e['sensordatavalues'] as List).map((e) {
+          var sd = e['value'];
+          double finalValue = 0;
+
+          if (sd is String) {
+            finalValue = double.parse(sd);
+          } else if (sd is double) {
+            finalValue = sd;
+          }
+
+          return SensorDataValue(
+            e['id'],
+            0,
+            e['value_type'],
+          );
+        }).toList(),
+      );
+    }).toList();
+  }
 }
